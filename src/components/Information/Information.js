70e3890b70a7841e454fe8b6712c3b01f3ee15ea
Andrii Salogub <andrejsalogub3@gmail.com>
2022-08-17 10:40:35 +0300

    add information output functionality

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./Information.scss";
 
 const Information = () => {
+    const employees = useSelector((state) => state.employees.employees);
+
+    let employeesWithAward = 0;
+
+    employees.forEach((element) => {
+        if (element.award === true) {
+            employeesWithAward += 1;
+        }
+    });
+
     return (
         <header className="information">
             <h1 className="information__title">
                 Учет сотрудников в компании N
             </h1>
             <h2 className="information__amount-of-employees">
-                Общее число сотрудников: 0
+                Общее число сотрудников: {Object.keys(employees).length}
             </h2>
-            <h2 className="information__on-award">Премию получать: 0</h2>
+            <h2 className="information__on-award">
+                Премию получать: {employeesWithAward}
+            </h2>
         </header>
     );
 };
