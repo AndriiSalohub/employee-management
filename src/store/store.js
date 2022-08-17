19133e35c17c29b30ad439ea4ab557e36c8c115b
Andrii Salogub <andrejsalogub3@gmail.com>
2022-08-17 10:13:58 +0300

    add search functionality

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import employeesSlice from "../slices/employeesSlice/employeesSlice";
+import searchSlice from "../slices/searchSlice/searchSlice";
 
 const store = configureStore({
     reducer: {
         employees: employeesSlice,
+        search: searchSlice,
     },
 });
 
